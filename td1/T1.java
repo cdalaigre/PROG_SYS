fb7e0605596c0d2edc83062133772585a79b380a
@cdalaigre <cdalaigre@gmail.com>
2023-09-12 22:34:20 +0200

    first commit

---
+++ td1/T1.java
@@ -0,0 +1,8 @@
+public class T1 extends Thread{
+    public void run(){
+        System.out.println("1");
+        System.out.println("2");
+        }    
+}
+
+
---
+++ td1/T2.java
@@ -0,0 +1,6 @@
+public class T2 extends Thread{
+    public void run(){
+        System.out.println("a");
+        System.out.println("b");
+        }    
+}
